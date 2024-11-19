e135fe35762b6ec171cca7bc8495ccb52c78524a
JessiePinkMaan <144606176+JessiePinkMaan@users.noreply.github.com>
2024-11-19 15:10:49 +0700

    Add files via upload

---
+++ Flyweight Pattern/Character.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight_Pattern
+{
+    public class Character
+    {
+        public string Name { 
+            get; 
+            private set; 
+        }
+
+        public string Type { 
+            get;
+            private set;
+        }
+
+        public string Image {
+            get;
+            private set;
+        }
+
+
+
+        private Character(string name, string type, string image)
+        {
+            Name = name;
+            Type = type;
+            Image = image;
+
+        }
+
+        // Внутренняя фабрика
+        public class CharacterFactory
+        {
+            private readonly Dictionary<string, Character> charlist = new Dictionary<string, Character>();//словарь для хранения ключей 
+        
+
+            public Character GetCharacter(string name, string type, string image)
+            {
+                string key = $"{name}_{type}";//создания ключа 
+
+                if (charlist.TryGetValue(key, out var existingCharacter))// ищем ключ в словаре или добавляем новый
+                {
+                    return existingCharacter;
+                }
+
+                var newC = new Character(name, type, image);
+                charlist[key] = newC;
+
+               
+                return newC ;
+            }
+        }
+    }
+}
---
+++ Flyweight Pattern/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Flyweight_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new Character.CharacterFactory();
+
+            // создаем перса и если такого персонажа нет фабрика создаст его  
+
+            var her1 = factory.GetCharacter("g", "zloi", "bledno.png");
+
+            // фабрика должна вернуть тот же объект что и her1
+            var her2 = factory.GetCharacter("g", "zloi", "hihihi.png"); // Этот персонаж будет тем же самым
+
+            Console.WriteLine($"перс 1: {her1.Name}, Type: {her1.Type}, Image: {her1.Image}");
+
+            Console.WriteLine($"перс 2: {her2.Name}, Type: {her2.Type}, Image: {her2.Image}");
+
+        }
+    }
+}
